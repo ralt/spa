62374cde459895ca91ab13accabe7a7068501697
Florian MARGAINE <florian@margaine.com>
2014-09-07 11:25:25 +0200

    Fixes the sql creation script.

--- base.sql
+++ base.sql
@@ -1,24 +1,24 @@
-CREATE TABLE cat (
+CREATE TABLE IF NOT EXISTS cat (
        id serial PRIMARY KEY,
        name varchar(255) UNIQUE NOT NULL,
        birthday date NOT NULL,
        presence boolean NOT NULL
 );
 
-CREATE TABLE type (
+CREATE TABLE IF NOT EXISTS type (
        id serial PRIMARY KEY,
        name varchar(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS users (
        id serial PRIMARY KEY,
        name varchar(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE history (
+CREATE TABLE IF NOT EXISTS history (
        cat bigint references cat(id),
        type bigint references type(id),
-       user bigint references user(id),
+       users bigint references users(id),
        comment text,
        time timestamp NOT NULL
 );
