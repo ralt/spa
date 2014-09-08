e0e4caa23ad05aaf17b1ff7507459ff5930b7310
Florian MARGAINE <florian@margaine.com>
2014-09-08 18:49:44 +0200

    Adds the status add form

--- status/db.lisp
+++ status/db.lisp
@@ -2,3 +2,6 @@
 
 (postmodern:defprepared status/get-all
     "SELECT id, name FROM status" :plists)
+
+(postmodern:defprepared-with-names status/create (name)
+  ("INSERT INTO status (name) VALUES ($1)" name) :none)
--- status/routes.lisp
+++ status/routes.lisp
@@ -1,2 +1,12 @@
 (in-package #:spa)
 
+(restas:define-route status/get-add ("status/add" :method :get)
+  (spa.layout:main
+   (list
+    :title "Add a status"
+    :body (spa.status:add))))
+
+(restas:define-route status/post-add ("status/add" :method :post)
+  (let* ((name (hunchentoot:post-parameter "name")))
+    (db status/create name)
+    (restas:redirect 'status/get-add)))
