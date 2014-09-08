c5238b61ce09a709c14a90ec827ad58de17804c5
Florian MARGAINE <florian@margaine.com>
2014-09-08 18:57:59 +0200

    Users add form

--- users/routes.lisp
+++ users/routes.lisp
@@ -1,6 +1,6 @@
 (in-package #:spa)
 
-(restas:define-route users/get-login ("user/login" :method :get)
+(restas:define-route users/get-login ("users/login" :method :get)
   (spa.layout:main
    (list
     :title "Login"
@@ -8,19 +8,19 @@
            (list
             :users (db users/get-all-names))))))
 
-(restas:define-route users/post-login ("user/login" :method :post)
+(restas:define-route users/post-login ("users/login" :method :post)
   (let ((id (hunchentoot:post-parameter "users")))
     (hunchentoot:start-session)
     (setf (hunchentoot:session-value "userid") id)
     (restas:redirect 'cat)))
 
-(restas:define-route users/get-add ("user/add" :method :get)
+(restas:define-route users/get-add ("users/add" :method :get)
   (spa.layout:main
    (list
     :title "Add user"
     :body (spa.users:add))))
 
-(restas:define-route users/post-add ("user/add" :method :post)
+(restas:define-route users/post-add ("users/add" :method :post)
   (let ((name (hunchentoot:post-parameter "name")))
     (db users/create name)
     (restas:redirect 'users/get-login)))
